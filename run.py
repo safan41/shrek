ad8d6cd5c321e2e49f6ee11be95f7ecf1876482c
Safwan Morshed <safan25641@gmail.com>
2018-06-06 19:24:25 -0500

    got bot to do what it should do, in the simplist way possible

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import discord
-
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+link = ["http://knowyourmeme.com/memes/", ]
 TOKEN = 'NDU0MDE3ODIwODUwOTEzMjg4.Dfnk9A._L7ChlbseOleX9vp-9ISUDCamvs'
 
 client = discord.Client()
-
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
@@ -13,7 +15,15 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
-
+    if message.content.startswith('!meme'):
+        mymsg = message.content[len('!meme'):].strip()
+        link.append(mymsg)
+        s = ''.join(link)
+        page = urlopen(s)
+        soup = BeautifulSoup(page, 'html.parser')
+        name_box = soup.find('section', attrs={'class': 'bodycopy'})
+        text = name_box.text.strip()
+        await client.send_message(message.channel, s)
 @client.event
 async def on_ready():
     print('Logged in as')
