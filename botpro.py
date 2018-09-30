1a5756ca947120e83e5693b59f0f147882836609
safan41 <safan25641@gmail.com>
2018-09-30 18:58:46 -0500

    finally

--- botpro.py
+++ botpro.py
@@ -1,6 +1,7 @@
 import discord
 from urllib.request import urlopen
 import re
+import os
 import praw
 from discord.ext import commands
 import computer
@@ -186,5 +187,5 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-bot.run('TOKEN')
+bot.run(os.environ['TOKEN'])
 # await client.send_message(message.channel, 'Error - No meme stats found for "' + mymsg + '"') if anything goes worng
